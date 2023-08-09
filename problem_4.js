935a91266db03e3db73e07f9b319377bd6916b11
omorfarukcode <omorfaruksarker999@gmail.com>
2023-08-09 10:32:54 +0600

    4d ok

--- problem_4.js
+++ problem_4.js
@@ -1,11 +1,19 @@
 function findAddress(obj) {
-    
-
+    if(typeof obj !== "object"){
+        return "please provide me an array of number"
+    }else {
+        const street = obj.street || "__";
+        const house = obj.house || "__";
+        const society = obj.society || "__";
+        return street + "," + house + "," + society;
+    }
 }
 const object = {
     street: 10,
     house:"15A",
     society:"Earth Perfect"
 }
-const result = findAddress(object);
-console.log(result);
+// const result = findAddress(object);
+// console.log(result);
+
+  
